d65993bdd2b4c494a3f6bb8dd48f5ec69a497f28
Konstantin Grachev <preemiere@ya.ru>
2016-01-25 12:17:00 +0300

    Remove register type

--- DependencyInjection/PreemiereCoreExtension.php
+++ DependencyInjection/PreemiereCoreExtension.php
@@ -2,8 +2,6 @@
 
 namespace Preemiere\CoreBundle\DependencyInjection;
 
-use Doctrine\DBAL\Types\Type;
-use Preemiere\CoreBundle\Doctrine\EnumType;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
@@ -16,23 +14,12 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  */
 class PreemiereCoreExtension extends Extension
 {
+    /**
+     * {@inheritdoc}
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-
-        if ($container->hasParameter('doctrine.entity_managers')) {
-            if (!Type::hasType(EnumType::ENUM)) {
-                Type::addType(EnumType::ENUM, EnumType::class);
-            }
-
-            $entityManagerNameList = $container->getParameter('doctrine.entity_managers');
-            foreach ($entityManagerNameList as $entityManagerName) {
-                $em = $container->get($entityManagerName);
-                if (!$em->getConnection()->getDatabasePlatform()->hasDoctrineTypeMappingFor(EnumType::ENUM)) {
-                    $em->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping(EnumType::ENUM, EnumType::ENUM);
-                }
-            }
-        }
     }
 }
